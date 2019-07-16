cb2414a63092af1ead319bef0980a6dd7f6fc24a
sam-malanchuk <malanchuksam@gmail.com>
2019-07-16 19:58:11 -0400

    imported connect to app and called api from componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
-
+import { getSwapi } from './actions/index'
+import { connect } from 'react-redux'
 import { CharacterListView } from "./views";
 import "./styles/App.css";
 
 class App extends Component {
+  componentDidMount() {
+    this.props.getSwapi()
+  }
   render() {
     return <CharacterListView />;
   }
 }
 
-export default App;
+const mapDispatchToProps = {
+  getSwapi,
+}
+
+export default connect(null, mapDispatchToProps)(App);
