8f6c5611c1ecc19d16fbff56910526d18ed17b9f
sam-malanchuk <malanchuksam@gmail.com>
2019-07-16 19:52:53 -0400

    imported applyMiddleware and created action creators

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,12 @@
-import /* we need our action types here*/ "../actions";
+import /* we need our action types here*/ {
+  FETCHING,
+  SUCCESS,
+  FAILURE,
+} from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  errorMessage: null,
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +14,15 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+    case FETCHING: {
+      return state;
+    }
+    case SUCCESS: {
+      return state;
+    }
+    case FAILURE: {
+      return state;
+    }
     default:
       return state;
   }
