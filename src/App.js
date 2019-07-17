aa8c532f47203c4ab6b06ea2274b0783c1464f0d
sam-malanchuk <malanchuksam@gmail.com>
2019-07-16 20:15:38 -0400

    removed connect from App, need to connect in CharacterListView

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import React, { Component } from "react";
-import { getSwapi } from './actions/index'
-import { connect } from 'react-redux'
 import { CharacterListView } from "./views";
 import "./styles/App.css";
 
 class App extends Component {
-  componentDidMount() {
-    this.props.getSwapi()
-  }
   render() {
     return <CharacterListView />;
   }
 }
 
-const mapDispatchToProps = {
-  getSwapi,
-}
-
-export default connect(null, mapDispatchToProps)(App);
+export default App;
